b83d5a10c2a0470f771ab8f570081bfe35029397
Victor Gil <victoredin94@hotmail.com>
2022-07-10 17:27:39 -0500

    fix: update files folder

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-envsubst '\${MESSAGE}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '\${MESSAGE}' < /etc/nginx/templates/default.conf.template > /etc/nginx/http.d/default.conf
 rm -v /etc/nginx/templates/default.conf.template
 exec nginx -g 'daemon off;'
